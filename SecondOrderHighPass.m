b23fd16fb9cbee41b3cb9a7d4d964872b659ff56
Ghani Rabbani <98215867+AmriRabban@users.noreply.github.com>
2022-02-20 11:55:25 +0700

    Update SecondOrderHighPass.m

--- SecondOrderHighPass.m
+++ SecondOrderHighPass.m
@@ -1,9 +1,12 @@
-%1st and 2nd Order High Pass Filter
+%% 1st and 2nd Order High-pass Filter
+%% Specify the resistor and capasitor value
 r1 = 11.6; %resistor 1
 r2 = 11.4; %resistor 2
 r3 = 11.6; %resistor 3
 r4 = 11.4; %resistor 4
 c = 1e-3; %capasitor
+
+%% Building a transfer function
 w = logspace(0,10,200); %frequency
 s = 1i*w; %differential operator
 w0 = 1/c*sqrt(r3*r4); %cut off frequency
@@ -13,7 +16,9 @@ x2 = s + w0; %denominator
 x = x1./x2; %transfer function of 1st order
 y1 = s.^2; %numerator
 y2 = (s.^2)+((w0/Q).*s)+(w0.^2); %denominator
-y = y1./y2; %transfer function
+y = y1./y2; %transfer function of 2nd order
+
+%% Plotting the high-pass filter
 p1 = plot(log10(w),20*log10(abs(x)),'red','Linewidth',1.5);
 hold on
 p2 = plot(log10(w),20*log10(abs(y)),'blue','Linewidth',1.5);
