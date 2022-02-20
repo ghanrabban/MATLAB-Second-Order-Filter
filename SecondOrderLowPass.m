cd8379614c27adbc2feb471c061574eb584e9a47
Ghani Rabbani <98215867+AmriRabban@users.noreply.github.com>
2022-02-20 11:56:23 +0700

    Update SecondOrderLowPass.m

--- SecondOrderLowPass.m
+++ SecondOrderLowPass.m
@@ -1,9 +1,12 @@
-%1st and 2nd Order Low Pass Filter
+%% 1st and 2nd Order Low-pass Filter
+%% Specify the resistor and capasitor value
 r1 = 12; %resistor 1
 r2 = 11; %resistor 2
 r3 = 12; %resistor 3
 r4 = 11; %resistor 4
 c = 1e-3; %capasitor
+
+%% Building a transfer function
 w = logspace(0,10,200); %frequency
 s = 1i*w; %differential operator
 w0 = 1/c*sqrt(r3*r4); %cut off frequency
@@ -14,6 +17,8 @@ x = x1./x2; %transfer function of 1st order
 y1 = w0^2; %numerator
 y2 = (s.^2)+((w0/Q).*s)+(w0.^2); %denominator
 y = y1./y2; %transfer function of 2nd order
+
+%% Plotting the low-pass filter
 p1 = plot(log10(w),20*log10(abs(x)),'red','Linewidth',1.5);
 hold on
 p2 = plot(log10(w),20*log10(abs(y)),'blue','Linewidth',1.5);
